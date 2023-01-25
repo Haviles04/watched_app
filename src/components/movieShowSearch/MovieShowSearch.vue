1d94dd3604da207747e6efc38c576ef2ee3ced11
haviles04 <haviles04@gmail.com>
2023-01-24 19:18:10 -0500

    implemented find something to watch

--- src/components/movieShowSearch/MovieShowSearch.vue
+++ src/components/movieShowSearch/MovieShowSearch.vue
@@ -1,7 +1,58 @@
+<script setup lang="ts">
+import ResultsCard from "./ResultsCard.vue";
+import { ref } from "vue";
+import placeHolder from "../../assets/placeholderImg.png";
+
+const { VITE_MOVIEDB_API_KEY } = import.meta.env;
+
+const searchTerm = ref<string>("");
+const searchedItems = ref<any[]>();
+let searchTimeout: any;
+
+const handleChange = () => {
+  clearTimeout(searchTimeout);
+  searchTimeout = setTimeout(() => {
+    fetchData();
+  }, 300);
+};
+
+const fetchData = async () => {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/search/multi?api_key=${VITE_MOVIEDB_API_KEY}&language=en-US&query=${searchTerm.value}&page=1&include_adult=false`
+  )
+    .then((r) => r.json())
+    .catch((err) => console.log(err));
+  searchedItems.value = data.results;
+  console.log(data.results);
+};
+</script>
+
 <template>
   <main>
     <div class="findContainer">
-      <h1>Find Something To Watch</h1>
+      <h1>Need Something To Watch?</h1>
+      <div class="searchBar">
+        <input
+          v-model="searchTerm"
+          type="text"
+          placeholder="Search for a movie or tv show..."
+          @input="handleChange"
+        />
+      </div>
+      <div class="cardContainer">
+        <ResultsCard
+          class="card"
+          v-for="item in searchedItems"
+          :title="item.title || item.name"
+          :releaseDate="item.release_date"
+          :overview="item.overview"
+          :image="
+            item.poster_path
+              ? `https://image.tmdb.org/t/p/w400/${item.poster_path}`
+              : placeHolder
+          "
+        />
+      </div>
     </div>
   </main>
 </template>
@@ -9,12 +60,54 @@
 <style scoped>
 .findContainer {
   display: flex;
-  flex: column;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 }
 
 .findContainer h1 {
   margin-top: 20px;
   color: white;
+}
+
+.cardContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+}
+
+.card {
+  margin: 20px;
+}
+
+.searchBar {
+  margin-top: 25px;
+  border-radius: 10px;
+  padding: 10px;
+  width: 300px;
+  background-color: #415a77;
+  color: white;
+}
+
+.searchBar input {
+  color: white;
+  width: 100%;
+  outline: none;
+}
+
+.searchBar input::placeholder {
+  color: white;
+}
+
+@media (max-width: 1020px) {
+  .cardContainer {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (max-width: 650px) {
+  .cardContainer {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
---
+++ src/components/movieShowSearch/ResultsCard.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const { title, releaseDate, overview, image } = defineProps([
+  "title",
+  "releaseDate",
+  "overview",
+  "image",
+]);
+</script>
+
+<template>
+  <v-card class="mx-auto card" max-width="300">
+    <v-img class="align-end text-white" height="400" :src="image" cover>
+    </v-img>
+
+    <v-card-subtitle class="pt-4">{{ releaseDate }} </v-card-subtitle>
+
+    <v-card-text>
+      <h1>{{ title }}</h1>
+      <div class="overView">{{ overview }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="white"> Details </v-btn>
+
+      <v-btn color="white"> Watched? </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+.card {
+  background-color: #415a77;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.card h1 {
+  color: white;
+  margin-bottom: 10px;
+  line-height: 1.7rem;
+}
+
+.overView {
+  max-height: 100px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+</style>
