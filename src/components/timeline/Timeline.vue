a49fec1bb876796f1e29583cac1a715ea3725bb5
haviles04 <haviles04@gmail.com>
2023-02-09 08:46:33 -0500

    error handling added

--- src/components/timeline/Timeline.vue
+++ src/components/timeline/Timeline.vue
@@ -6,31 +6,38 @@ import { storeToRefs } from "pinia";
 import TimelineCard from "./TimelineCard.vue";
 import TimelineSearch from "./TimelineSearch.vue";
 import Loading from "../Loading.vue";
+import Error from "../Error.vue";
 
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
 const posts = ref<{}[]>();
 const loading = ref(false);
 const emptyPost = ref(false);
+const error = ref(false);
 
 const fetchData = async () => {
   loading.value = true;
-  const { data: following, error } = await supabase
-    .from("follower_following")
-    .select()
-    .eq("follower_id", user.value?.id);
+  try {
+    const { data: following } = await supabase
+      .from("follower_following")
+      .select()
+      .eq("follower_id", user.value?.id);
 
-  const ownerIds = following?.map((f) => f.following_id);
+    const ownerIds = following?.map((f) => f.following_id);
 
-  const { data: postsData } = await supabase
-    .from("post")
-    .select()
-    .in("owner_id", ownerIds!)
-    .order("created_at", { ascending: false });
+    const { data: postsData } = await supabase
+      .from("post")
+      .select()
+      .in("owner_id", ownerIds!)
+      .order("created_at", { ascending: false });
 
-  posts.value = postsData!;
-  emptyPost.value = posts.value.length < 1;
-  loading.value = false;
+    posts.value = postsData!;
+    emptyPost.value = posts.value.length < 1;
+    loading.value = false;
+  } catch {
+    loading.value = false;
+    error.value = true;
+  }
 };
 
 onMounted(() => {
@@ -39,14 +46,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <TimelineSearch />
-  <div class="emptyPost" v-if="emptyPost">
-    <span>Add Friends to see what they have watched!</span>
+  <div v-if="!error">
+    <TimelineSearch />
+    <div class="emptyPost" v-if="emptyPost">
+      <span>Add Friends to see what they have watched!</span>
+    </div>
+    <div v-if="!loading" class="cardContainer">
+      <TimelineCard v-for="post in posts" :post="post" />
+    </div>
+    <div v-else><Loading /></div>
   </div>
-  <div v-if="!loading" class="cardContainer">
-    <TimelineCard v-for="post in posts" :post="post" />
+  <div v-else>
+    <Error />
   </div>
-  <div v-else><Loading /></div>
 </template>
 
 <style scoped>
